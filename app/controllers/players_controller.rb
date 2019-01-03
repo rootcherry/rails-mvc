c72de317c7fe4da1f42022e2fd529873eb25ad28
Chris N <chriss.noga@gmail.com>
2019-01-02 22:16:38 -0200

    update a existing player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -24,6 +24,16 @@ class PlayersController < ApplicationController
 		@player = Player.find(params[:id])
 	end
 
+	def update
+		player = Player.find(params[:id])
+		if player.update(player_params)
+			redirect_to "/players"
+		else
+			flash[:errors] = player.errors.full_messages
+			redirect_to "/players/#{player.id}/edit"
+		end
+	end
+
 private 
 	def player_params
 		params.require(:player).permit(:first_name, :last_name)
